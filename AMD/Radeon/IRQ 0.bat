6a7835327faea983251a1d5abe03d186deb67183
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-05 17:52:24 -0600

    Update IRQ 0.bat

--- AMD/Radeon/IRQ 0.bat
+++ AMD/Radeon/IRQ 0.bat
@@ -2,5 +2,5 @@
 wmic path Win32_PnPEntity where "name='Temporizador de eventos de alta precisión'" call disable
 wmic path Win32_PnPEntity where "name='High precision event timer'" call disable
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\msisadrv" /v Start /t REG_DWORD /d 4 /f
-Los cambios se completaron con exito.
+echo Los cambios se completaron con exito. Reinicie su sistema.
 pause
