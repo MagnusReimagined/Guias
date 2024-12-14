f088203033c06707bc1e986bea70fb008401a218
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-14 00:33:38 -0600

    Update RadeonTweaks.bat

--- AMD/Radeon/RadeonTweaks.bat
+++ AMD/Radeon/RadeonTweaks.bat
@@ -1,18 +1,24 @@
 @echo off
 
 echo.
-echo Activando P0
+echo Forzando P0 y desactivando ULPS...
+
+set "cambioRealizado=0"
+
 for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
-    for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
-        for /f %%i in ('echo %%a ^| findstr "{"') do (
-            Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%i" /v "DisableDynamicPstate" /t REG_DWORD /d "1" /f > nul 2>&1
+    for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver" 2^>nul') do (
+        for /f %%j in ('echo %%a ^| findstr "{"') do (
+            reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "DisableDynamicPstate" /t REG_DWORD /d 1 /f >nul 2>&1
+            reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "EnableUlps" /t REG_DWORD /d 0 /f >nul 2>&1
+            reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f >nul 2>&1
+
             set "cambioRealizado=1"
         )
     )
 )
 
-if %cambioRealizado%==1 (
-    echo La operacion se completo correctamente.
+if "%cambioRealizado%"=="1" (
+    echo La operación se completó correctamente.
 ) else (
     echo No se realizaron cambios.
 )
@@ -29,11 +35,6 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v
 echo.
 echo Desactivando HAGS
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d 1 /f
-
-echo.
-echo Desactivando ULPS
-Reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps" /t REG_DWORD /d 0 /f
-Reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f
 
 echo.
 Echo Cambios hechos, reinicia tu perfil para aplicar los cambios.
