03d7eecb5f6b539c588d1b1f60cfd34e327e7fbb
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-14 04:46:10 -0600

    Update IRQ 0.bat

--- AMD/Radeon/IRQ 0.bat
+++ AMD/Radeon/IRQ 0.bat
@@ -1,6 +1,13 @@
 @echo off
+echo.
+echo Desactivando HPET
 wmic path Win32_PnPEntity where "name='Temporizador de eventos de alta precisión'" call disable
 wmic path Win32_PnPEntity where "name='High precision event timer'" call disable
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\msisadrv" /v Start /t REG_DWORD /d 4 /f
-echo Los cambios se completaron con exito. Reinicie su sistema.
+
+echo.
+echo Desinstalando msisadrv
+reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\msisadrv" /f
+
+echo.
+echo Cambios hechos, reinicie su sistema.
 pause
