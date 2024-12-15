b92e179ec551741e490b0f44c139efa94527a940
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-14 21:50:50 -0600

    Update RadeonTweaks.bat

--- AMD/Radeon/RadeonTweaks.bat
+++ AMD/Radeon/RadeonTweaks.bat
@@ -1,33 +1,30 @@
 @echo off
 
 echo.
-echo Forzando P0
-:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "DisableDynamicPstate" /t REG_DWORD /d 1 /f
-
-echo.
-echo Desactivando ULPS
-:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps" /t REG_DWORD /d 0 /f 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f 
-
-echo.
-echo Activando ShaderCache
-:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000\UMD" /v "ShaderCache" /t REG_BINARY /d 3200 /f 
+echo Activando DisableDynamicPstate, ShaderCache y desactivando ULPS
+for /f %%i in ('wmic path Win32_VideoController get PNPDeviceID^| findstr /L "PCI\VEN_"') do (
+    for /f "tokens=3" %%a in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%i" /v "Driver"') do (
+        for /f %%j in ('echo %%a ^| findstr "{"') do (
+            Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "DisableDynamicPstate" /t REG_DWORD /d "1" /f > nul 2>&1
+            Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "EnableUlps" /t REG_DWORD /d "0" /f > nul 2>&1
+            Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j" /v "EnableUlps_NA" /t REG_DWORD /d "0" /f > nul 2>&1
+            Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\%%j\UMD" /v "ShaderCache" /t REG_BINARY /d "3200" /f > nul 2>&1
+        )
+    )
+)
 
 echo.
 echo Desactivando MPO
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_DWORD /d 5 /f 
+reg add "HKLM\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_DWORD /d 5 /f 
 
 echo.
 echo Configurando TDR
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrLevel" /t REG_DWORD /d 3 /f 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDelay" /t REG_DWORD /d 8 /f 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrLevel" /t REG_DWORD /d 3 /f 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDelay" /t REG_DWORD /d 8 /f 
 
 echo.
 echo Activando HAGS
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d 1 /f
 
 echo.
 echo Todos los cambios han sido realizados. Reinicia tu PC para que los cambios surtan efecto.
