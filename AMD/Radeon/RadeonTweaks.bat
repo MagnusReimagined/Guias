443437be8294815986809d1f7dc84f1774fa5838
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-14 21:35:47 -0600

    Update RadeonTweaks.bat

--- AMD/Radeon/RadeonTweaks.bat
+++ AMD/Radeon/RadeonTweaks.bat
@@ -3,18 +3,18 @@
 echo.
 echo Forzando P0
 :: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "DisableDynamicPstate" /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "DisableDynamicPstate" /t REG_DWORD /d 1 /f
 
 echo.
 echo Desactivando ULPS
 :: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps" /t REG_DWORD /d 0 /f 
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps" /t REG_DWORD /d 0 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f 
 
 echo.
 echo Activando ShaderCache
 :: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000\UMD" /v "ShaderCache" /t REG_BINARY /d 3200 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000\UMD" /v "ShaderCache" /t REG_BINARY /d 3200 /f 
 
 echo.
 echo Desactivando MPO
