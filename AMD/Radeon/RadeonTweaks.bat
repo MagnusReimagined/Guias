d3f19f226515714f014f5a615c57f2d0073d78a7
MagnusTweaker <166988915+MagnusTweaker@users.noreply.github.com>
2024-12-14 21:33:57 -0600

    Update and rename RadeonTweaks.ps1 to RadeonTweaks.bat

---
+++ AMD/Radeon/RadeonTweaks.bat
@@ -0,0 +1,34 @@
+@echo off
+
+echo.
+echo "Forzando P0
+:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "DisableDynamicPstate" /t REG_DWORD /d 1 /f
+
+echo.
+echo Desactivando ULPS
+:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps" /t REG_DWORD /d 0 /f 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "EnableUlps_NA" /t REG_DWORD /d 0 /f 
+
+echo.
+echo Activando ShaderCache
+:: Remplaza 0000 por 0001, 0002, 0003, dependera de su sistema.
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Class\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000\UMD" /v "ShaderCache" /t REG_BINARY /d 3200 /f 
+
+echo.
+echo Desactivando MPO
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Dwm" /v "OverlayTestMode" /t REG_DWORD /d 5 /f 
+
+echo.
+echo Configurando TDR
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrLevel" /t REG_DWORD /d 3 /f 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDelay" /t REG_DWORD /d 8 /f 
+
+echo.
+echo Activando HAGS
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "HwSchMode" /t REG_DWORD /d 1 /f
+
+echo.
+echo Todos los cambios han sido realizados. Reinicia tu PC para que los cambios surtan efecto.
+pause
